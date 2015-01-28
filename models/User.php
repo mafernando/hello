8c092697e8d6c9e98fd669255e8c2c299f7b16f7
Jeff Reifman <jeff@newscloud.com>
2015-01-28 11:50:44 -0800

    Updated user model for final tutorial change

--- models/User.php
+++ models/User.php
@@ -5,6 +5,9 @@ use dektrium\user\models\User as BaseUser;
 
 class User extends BaseUser
 {
+  const ROLE_USER = 10;
+  const ROLE_MODERATOR = 20;
+  const ROLE_ADMIN = 30;
   
     public function register()
     {
