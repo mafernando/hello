50fcf54b1edc292eb63ac215f38838b7038f2e42
Jeff Reifman <jeff@newscloud.com>
2015-01-29 14:11:29 -0800

    Programming With Yii2: Asset Bundles Tutorial

--- views/status/_form.php
+++ views/status/_form.php
@@ -3,6 +3,9 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
+use app\assets\StatusAsset;
+StatusAsset::register($this);
+
 /* @var $this yii\web\View */
 /* @var $model app\models\Status */
 /* @var $form yii\widgets\ActiveForm */
@@ -10,22 +13,29 @@ use yii\widgets\ActiveForm;
 
 <div class="status-form">
     <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'message')->widget(\yii\redactor\widgets\Redactor::className(),
-    [
-       'clientOptions' => [
-           'imageUpload' => \yii\helpers\Url::to(['/redactor/upload/image']),
-       ],
-    ]
-      ) ?>
-
+    
+    <div class="row">
+      <div class="col-md-8">
+      <?= $form->field($model, 'message')->textarea(['rows' => 6]) ?>
+      </div>
+      <div class="col-md-4">
+      <p>Remaining: <span id="counter2">0</span></p>
+      </div>
+    </div>
+    <div class="row">
+    <div class="clearfix col-md-12">
+      
     <?=
         $form->field($model, 'permissions')->dropDownList($model->getPermissions(), 
                  ['prompt'=>Yii::t('app','- Choose Your Permissions -')]) ?>
+               </div>
+    </div>
+    <div class="row">
                  
     <div class="form-group">
  <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
      </div>
+   </div>
 
     <?php ActiveForm::end(); ?>
 
